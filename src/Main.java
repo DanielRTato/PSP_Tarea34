adad8b4d7f973706316c7fdd87ea3f6a1fc7afad
DanielRTato <drodrigueztato@danielcastelao.org>
2025-12-05 21:46:36 +0100

    Tarea 33 terminada

--- src/Main.java
+++ src/Main.java
@@ -10,34 +10,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("introduce una URL");
-        String url = scanner.nextLine();
-
-        HttpClient cliente = HttpClient.newHttpClient(); // Crear el cliente HTTP
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url)) // dirección del recurso
-                .GET() // pide información
-                .timeout(Duration.ofSeconds(5L)) // Tiempo de espera de 5 segundos
-                .build();
-
-        try {
-            HttpResponse<String> response = cliente.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("El código de estado es: " + response.statusCode());
-            System.out.println("El tipo de contenido es: " + response.headers().firstValue("content-type").orElse("Desconocido"));
-
-            if (response.statusCode() == 200) {
-                try (PrintWriter escritor = new PrintWriter("inspector.html");) {
-                    System.out.println("La petición fue exitosa, guardando el HTML en un fichero...");
-
-                    escritor.write(response.body());
-                }
-            }
-
-        } catch (IOException | InterruptedException e) {
-            System.out.println("Error al realizar la petición: " + e.getMessage());
-        }
     }
 }
